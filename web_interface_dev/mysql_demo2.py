c21e7ce8b686a2cebd21a2f4f4086590709f66fa
yanxi <yanxi@globalroam.com>
2017-06-12 18:13:14 +0800

    20170612

--- web_interface_dev/mysql_demo2.py
+++ web_interface_dev/mysql_demo2.py
@@ -15,12 +15,6 @@ conn = connect(host='127.0.0.1',
 
 try:
     with conn.cursor() as cursor:
-        # create guest data
-        sql = 'INSERT INTO sign_guest (realname, phone, email, sign, event_id, create_time) VALUES ("tom",18800110002,"tom@mail.com",0,1,NOW());'
-        cursor.execute(sql)
-    conn.commit()
-
-    with conn.cursor() as cursor:
         # query added guest
         sql = "SELECT realname,phone,email,sign FROM sign_guest WHERE phone=%s"
         cursor.execute(sql,('18800110002',))

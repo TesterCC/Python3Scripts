21a87f0ccda1257613db6eb5f748296b6b5d8fa4
LYX <testerlyx@foxmail.com>
2017-07-23 20:03:54 +0800

    20170723_01, finish python advance 2-1, how to select data in list, dict and set.

--- imooc/python_advance_py3/2_1_1.py
+++ imooc/python_advance_py3/2_1_1.py
@@ -5,7 +5,31 @@
 # 2-1 如何在列表, 字典, 集合中根据条件筛选数据
 
 from random import randint
+import time
 
 data = [randint(-10, 10) for _ in range(10)]   # python3 on xrange()
 print("Random Int Data List >>>> "+str(data))  # data is List class, need to convert
 print("--------------------------------")
+
+# Method 1 -- 使用filter函数，过滤掉负数
+start = time.clock()
+r1 = filter(lambda x: x >= 0, data)
+end = time.clock()
+print("R1 run time: %f" % (end-start))
+print(list(r1))     # in python3, filter() return need user list() to display
+
+
+# Method 2 -- 使用列表解析，过滤掉负数 -- quicker -- 更快，故首选列表解析
+start = time.clock()
+r2 = [x1 for x1 in data if x1 >= 0]
+end = time.clock()
+print("R2 run time: %f" % (end-start))
+print(r2)
+print("--------------------------------")
+
+# Method 4 -- 筛出集合{77,89,32,20...}中能被3整除的元素   集合Set
+s = set(data)
+print(s)
+result = {x for x in s if x % 3 == 0}
+print(result)
+
---
+++ imooc/python_advance_py3/2_1_3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+# http://coding.imooc.com/lesson/62.html#mid=917
+# 2-1 如何在列表, 字典, 集合中根据条件筛选数据
+
+from random import randint
+
+
+# 筛出字典{'Lilei':79, 'Lucy':92 ...}中值高于90的项
+# 随机生成一个字典
+d = {x: randint(60, 100) for x in range(1, 21)}
+print(d)
+
+

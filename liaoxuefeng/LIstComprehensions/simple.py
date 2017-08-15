d729add9a0211eb980839b4ff794045c77ea36a7
LYX <testerlyx@foxmail.com>
2017-08-15 23:24:56 +0800

    20170815_01, add ListComprehensions demo.

---
+++ liaoxuefeng/LIstComprehensions/simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+# 列表生成式
+# https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/001431779637539089fd627094a43a8a7c77e6102e3a811000
+
+
+# 要生成list [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(list(range(1,11)))

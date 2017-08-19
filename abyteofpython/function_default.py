73fc555a466f96ad31096cbe4e87e71dbb0af57b
LYX <testerlyx@foxmail.com>
2017-08-19 23:11:10 +0800

    20170819_01, add default param demo.

---
+++ abyteofpython/function_default.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# A Byte of Python -- P62 -- Python3
+
+
+def say(message, times=1):
+    print(message * times)
+
+
+if __name__ == '__main__':
+    say("Hello")
+    say("World", 5)

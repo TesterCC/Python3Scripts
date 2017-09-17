9aa3ac6a59ae9e9dc2a717e80b0d5f5b92b560bb
LYX <testerlyx@foxmail.com>
2017-09-17 23:08:23 +0800

    20170917_01, add import module demo.

---
+++ abyteofpython/function_import.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# __author__ = 'TesterCC'
+# __time__   = '17/9/17 23:02'
+
+
+# A Byte of Python -- Python3 -- P70
+
+from math import sqrt
+
+print("Square root of 16 is ", sqrt(16))    # 求16开方的结果，4
+
---
+++ abyteofpython/module_using_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# __author__ = 'TesterCC'
+# __time__   = '17/9/17 23:01'
+
+
+# A Byte of Python -- Python3 -- P70
+
+if __name__ == '__main__':
+    print("This program is being run by itself.")
+else:
+    print('I am being imported from another module.')
+
+
+

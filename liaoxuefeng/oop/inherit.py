372ede417c99c1c06828bbc26ff6ee6cf4d0621c
LYX <testerlyx@foxmail.com>
2017-11-14 20:02:43 +0800

    20171114_01, add class demo.

---
+++ liaoxuefeng/oop/inherit.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# __author__ = 'TesterCC'
+# __time__   = '17/11/14 19:44'
+
+"""
+https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/001431865288798deef438d865e4c2985acff7e9fad15e3000
+继承和多态
+多态（polymorphism）
+
+在OOP程序设计中，当我们定义一个class的时候，可以从某个现有的class继承，新的class称为子类（Subclass），
+而被继承的class称为基类、父类或超类（Base class、Super class）。
+"""
+
+
+class Animal(object):
+    def run(self):
+        print('Animal is running ...')
+
+
+# 对于Dog来说，Animal就是它的父类，对于Animal来说，Dog就是它的子类。Cat和Dog类似。
+class Dog(Animal):
+    # 也可以对子类增加一些方法，比如Dog类
+    def run(self):
+        print('Dog is running ...')
+
+    def eat(self):
+        print('Eating meat ...')
+
+
+class Cat(Animal):
+    def run(self):
+        print('Cat is running ...')
+
+# 当子类和父类都存在相同的run()方法时，我们说，子类的run()覆盖了父类的run()，在代码运行的时候，总是会调用子类的run()。
+# 这样，我们就获得了继承的另一个好处：多态。
+
+if __name__ == '__main__':
+    dog = Dog()
+    dog.run()
+    cat = Cat()
+    cat.run()
--- liaoxuefeng/oop/protected_student.py
+++ liaoxuefeng/oop/protected_student.py
@@ -8,3 +8,45 @@ https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c
 访问限制
 """
 
+# 如果要让内部属性不被外部访问，可以把属性的名称前加上两个下划线__，
+# 在Python中，实例的变量名如果以__开头，就变成了一个私有变量（private），只有内部可以访问，外部不能访问
+
+
+class Student(object):
+
+    def __init__(self, name, score):
+        self.__name = name
+        self.__score = score
+
+    def print_score(self):
+        print('%s:%s' % (self.__name, self.__score))
+
+# 但是如果外部代码要获取name和score怎么办？可以给Student类增加get_name和get_score这样的方法
+    def get_name(self):
+        return self.__name
+
+    def get_score(self):
+        return self.__score
+
+# 如果又要允许外部代码修改score怎么办？可以再给Student类增加set_score方法
+# 因为在方法中，可以对参数做检查，避免传入无效的参数
+    def set_score(self, score):
+        if 0 <= score <= 100:
+            self.__score = score
+        else:
+            raise ValueError('bad score, please check it')
+
+if __name__ == '__main__':
+    bart = Student('Bart Simpson', 98)
+    print(bart.get_name())
+    print(bart.get_score())
+    bart.set_score(63)
+    print(bart.get_score())
+    print(bart._Student__name)
+
+# 双下划线开头的实例变量是不是一定不能从外部访问呢？其实也不是。
+# 不能直接访问__name是因为Python解释器对外把__name变量改成了_Student__name，
+# 所以，仍然可以通过_Student__name来访问__name变量
+# 但是强烈建议你不要这么干，因为不同版本的Python解释器可能会把__name改成不同的变量名
+
+

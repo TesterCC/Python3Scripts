c9c0e25d855ba8752947a98b2c0179186dab06f7
MFC <testerlyx@foxmail.com>
2018-01-13 23:23:35 +0800

    20180113_01, add for loop demo.

---
+++ kaiyuanyouce/python3basic/python3_forloop.py
@@ -0,0 +1,53 @@
+# -*- coding:utf-8 -*-
+__author__ = 'MFC'
+__time__ = '18/1/13 23:17'
+
+"""
+Python3 for循环
+https://mp.weixin.qq.com/s?__biz=MzI0NDQ5NzYxNg==&mid=2247484031&idx=1&sn=669f92bf80d276b116225d147ab4d14b&scene=19#wechat_redirect
+"""
+
+
+def tuple_for():
+    """
+    for循环如何进行元组遍历输出
+    """
+    tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+
+    print(u"遍历元组，并打印出来： ")
+    for t in tuple:
+        print(t)
+
+
+def list_for():
+    """
+    用for循环如何进行列表遍历输出
+    """
+    list0 = [u'LearnTest', u'测试', u'快学Python3']
+
+    print(u"遍历列表，并打印出来： ")
+    for text in list0:
+        print(text)
+
+
+def dict_for():
+    """
+    用for循环如何进行字典遍历输出
+    """
+    dict = {u"开源": u"LearnTest", u"python": u"快学Python3"}
+
+    print(u"遍历字典方式一，并打印出来:")
+    for key, value in dict.items():
+        print("%s : %s " % (key, value))
+
+    print("\n-----------------------------")
+
+    print(u"遍历字典方式二，并打印出来: ")
+    for key in dict:
+        print("%s : %s " % (key, dict[key]))
+
+
+if __name__ == '__main__':
+    tuple_for()
+    list_for()
+    dict_for()

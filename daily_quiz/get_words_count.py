011b32fea0a542093d7f698398674eb757646ad6
MFC <liyanxi07@gmail.com>
2018-03-17 00:46:44 +0800

    add: 2 daily quiz.

---
+++ daily_quiz/get_words_count.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'MFC'
+__time__ = '18/3/17 00:44'
+
+"""
+每日一题 2018年3月12日
+
+统计下面这个文本中每个单词的出现次数（词频）：
+http://textfiles.com/stories/100west.txt
+"""
+

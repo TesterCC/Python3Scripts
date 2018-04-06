af1aeb1671f8778606db11e7aed6267882786e88
MFC <liyanxi07@gmail.com>
2018-04-06 23:58:36 +0800

    add: get ajax page data demo.

---
+++ python_network_crawler/p44_get_ajax_page_url.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'MFC'
+__time__ = '18/4/6 23:53'
+
+"""
+Python网络爬虫--从入门到实践   P42-44
+解析真实地址抓取
+
+TARGET_URL = "http://www.santostang.com/2017/03/02/hello-world/"
+User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36
+
+"""
+

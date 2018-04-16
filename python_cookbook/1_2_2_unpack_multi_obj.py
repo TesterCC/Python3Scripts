73dea2ef9a9049a14f73fda2638e274535c97cf0
MFC <liyanxi07@gmail.com>
2018-04-16 23:49:48 +0800

    add: unpack demo.

--- python_cookbook/1_2_2_unpack_multi_obj.py
+++ python_cookbook/1_2_2_unpack_multi_obj.py
@@ -10,3 +10,25 @@ P16
 """
 
 # 星号表达式在迭代元素为可变长元组的序列时是很有用的。
+
+records = [
+    ('foo', 1, 2),
+    ('bar', 'hello'),
+    ('foo', 3, 4),
+]
+
+
+def do_foo(x, y):
+    print('foo', x, y)
+
+
+def do_bar(s):
+    print('bar', s)
+
+
+for tag, *args in records:
+    if tag == 'foo':
+        do_foo(*args)
+    elif tag == 'bar':
+        do_bar(*args)
+

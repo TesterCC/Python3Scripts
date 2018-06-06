954ff227888ef3d06c34a29d86aed6b32e9f0ba9
MFC <liyanxi07@gmail.com>
2018-06-06 09:15:18 +0800

    update c2_2.py

--- imooc/python3_shizhan/pythonic/c2_2.py
+++ imooc/python3_shizhan/pythonic/c2_2.py
@@ -4,7 +4,7 @@ __author__ = 'MFC'
 __time__ = '18/6/5 22:32'
 
 """
-14章
+14章 Pythonic与Python杂记
 """
 
 
@@ -20,14 +20,19 @@ def get_tuesday():
     return 'Tuesday'
 
 
+def get_default():
+    return 'Unknown'
+
+
 switcher = {
     0: get_sunday,  # 注意这里是函数，不是函数的调用
     1: get_monday,
     2: get_tuesday,
 }
 
-day = 1
+# day = 1
+day = 8
 
-day_name = switcher.get(day, 'Unknown')()
+day_name = switcher.get(day, get_default)()
 
 print(day_name)

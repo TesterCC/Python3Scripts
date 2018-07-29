789eaa755c604d66d9f536653a7a4302ac26ee2f
yanxi <testerlyx@foxmail.com>
2018-07-30 00:43:31 +0800

    add: update mysqlclient python demo code.

--- imooc/python3_database/python3mysql_conn.py
+++ imooc/python3_database/python3mysql_conn.py
@@ -2,3 +2,36 @@
 # -*- coding:utf-8 -*-
 __author__ = 'MFC'
 __time__ = '18/7/29 23:58'
+
+"""
+https://mysqlclient.readthedocs.io/
+http://www.runoob.com/python/python-mysql.html
+
+pip install mysqlclient
+pip install mysqlclient -i https://pypi.douban.com/simple/
+"""
+
+import MySQLdb
+
+# prepare sql
+sql = 'SELECT * FROM news;'
+
+# get connect
+conn = MySQLdb.connect(
+    host='127.0.0.1',
+    user='root',
+    password='yanxi76543210',
+    db='news',
+    port=3306,
+    charset='utf8'
+)
+
+# get data
+cursor = conn.cursor()
+cursor.execute(sql)
+# ret = cursor.fetchone()
+ret = cursor.fetchall()
+print(ret)
+
+# close data
+conn.close()

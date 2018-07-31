ab8349d99af0eb860b1217c15b40fc50fa2b1d4f
yanxi <testerlyx@foxmail.com>
2018-07-31 13:41:12 +0800

    add: python3 mysql demo

--- imooc/python3_database/mysql_conn2.py
+++ imooc/python3_database/mysql_conn2.py
@@ -16,21 +16,26 @@ import MySQLdb
 # prepare sql
 sql = 'SELECT * FROM news;'
 
-# get connect
-conn = MySQLdb.connect(
-    host='127.0.0.1',
-    user='root',
-    password='',
-    db='news',
-    port=3306,
-    charset='utf8'
-)
+try:
+
+    # get connect
+    conn = MySQLdb.connect(
+        host='127.0.0.1',
+        user='root',
+        password='',
+        db='news',
+        port=3306,
+        charset='utf8'
+    )
+
+except MySQLdb.Error as e:
+    print("Error: %s" % e)
 
 # get data
 cursor = conn.cursor()
-cursor.execute(sql)
-# ret = cursor.fetchone()
-ret = cursor.fetchall()
+cursor.execute('SELECT * FROM news ORDER BY created_at DESC;')
+ret = cursor.fetchone()
+# ret = cursor.fetchall()
 print(ret)
 
 # close data

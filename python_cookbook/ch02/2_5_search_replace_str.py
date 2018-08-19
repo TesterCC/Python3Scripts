042840338aa604c1155da4f5cd3a786d38bd0d0e
yanxi <testerlyx@foxmail.com>
2018-08-19 23:00:40 +0800

    add: search and replace str demo.

---
+++ python_cookbook/ch02/2_5_search_replace_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'MFC'
+__time__ = '18/8/19 22:57'
+
+"""
+《Python+Cookbook》第三版中文v3.0.0   
+2.5 字符串搜索和替换    P55
+
+问题:
+在字符串中搜索和匹配指定的文本模式
+
+
+"""
+

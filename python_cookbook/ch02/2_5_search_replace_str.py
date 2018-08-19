037d41c47075b18ed5b455f0667246f02552bc5c
yanxi <testerlyx@foxmail.com>
2018-08-20 02:02:09 +0800

    update demo.

--- python_cookbook/ch02/2_5_search_replace_str.py
+++ python_cookbook/ch02/2_5_search_replace_str.py
@@ -9,7 +9,22 @@ __time__ = '18/8/19 22:57'
 
 问题:
 在字符串中搜索和匹配指定的文本模式
-
-
 """
 
+# simple mode, use replace()
+
+text = 'Go is a wonderful language, Go is my favorite, You should learn Go'
+
+ret = text.replace("Go", "Python")
+
+print(ret)
+
+# complex mode, use re.sub()
+
+text = 'Today is 08/20/2018, Python practice starts 10/24/2018.'
+
+import re
+
+ret = re.sub(r'(\d+)/(\d+)/(\d+)', r'\3-\1-\2', text)
+
+print(ret)

41e678324e129bc461b0a9f9dacb2376675462c0
yanxi <testerlyx@foxmail.com>
2018-09-11 23:49:21 +0800

    add: update meta_info

---
+++ python_cookbook/ch07/7_3_add_meta_info.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'MFC'
+__time__ = '18/9/11 23:43'
+
+"""
+《Python+Cookbook》第三版中文v3.0.0   
+7.3 给函数参数增加元信息    P217-218
+使用函数参数注解
+"""
+
+
+def add(x: int, y: int) -> int:
+    return x + y
+
+
+# Python解释器不会对注解添加语义
+
+print(add(3, 7))
+
+help(add)

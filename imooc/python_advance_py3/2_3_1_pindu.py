cb630ad2d162bd5dd2a50dc5b9a0dc4f413a1885
yanxi <testerlyx@foxmail.com>
2018-10-29 14:31:06 +0800

    update python3 usage.

--- imooc/python_advance_py3/2_3_1_pindu.py
+++ imooc/python_advance_py3/2_3_1_pindu.py
@@ -12,22 +12,55 @@ data = [randint(0, 20) for _ in range(30)]
 print(data)
 
 # data作key，0作value
-c = dict.fromkeys(data, 0)    # 方法用于创建一个新的字典
-print(c)
+d = dict.fromkeys(data, 0)    # 方法用于创建一个新的字典, 这个字典中的key为data中的元素，值全是0
+print(d)
 
 # 统计出现次数
 for x in data:
-    c[x] += 1
+    d[x] += 1
 
-print(c)
-print("------------------------------")
+print(d)
 
+print("-----------根据频度排序，降序-------------------")
+
+print("="*15 + "method 1" + "="*15)
+# ret = sorted([(v, k) for k, v in d.items()], reverse=True)
+
+ret = sorted(((v, k) for k, v in d.items()), reverse=True)
+# 用生成器解析比列表解析更节约空间
+# 但这种方法总得来说不好，需要对整个列表进行排序
+
+print(ret)
+
+print("----找到出现次数最高的3个元素----")
+print(ret[:3])
+
+print("="*15 + "method 2" + "="*15)
+# Python3实用编程技巧进阶 2-4 如何统计序列中元素的频度 04:24
+# 在很大的列表中找到前3个，通常使用堆这个数据结构
+
+import heapq
+
+ret1 = heapq.nlargest(3, ((v, k) for k, v in d.items()))
+
+ret2 = heapq.nsmallest(3, ((v, k) for k, v in d.items()))
+
+print("----找到出现次数最高的3个元素----")
+print(ret1)
+
+print("----找到出现次数最低的3个元素----")
+print(ret2)
+
+
+print("="*15 + "method 3" + "="*15)
+print("----找到出现次数最高的3个元素----")
 # 找到出现次数最高的3个元素
 # 一般来说就是对字典data中的值进行排序
-# Here use collections.Counter object.
+# Here use collections.Counter object.    05:25
 c2 = Counter(data)     # 词频统计结果
 # print(c2[10])
 
+print(c2)
 res = c2.most_common(3)    # 找到出现频度最高的三个元素  list object
 print("出现次数最高的3个(元素:次数): "+str(res))
 

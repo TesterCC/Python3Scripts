6e311ea081ca2d899e048cf0c0b8e4bb085f64a9
yanxi <testerlyx@foxmail.com>
2018-12-03 00:53:35 +0800

    add: Tornado templates demo.

--- imooc/tornado_web/hello_world.py
+++ imooc/tornado_web/hello_world.py
@@ -3,6 +3,9 @@
 __author__ = 'MFC'
 __time__ = '18/11/30 13:36'
 
+import os
+import datetime
+
 import tornado.ioloop
 import tornado.web
 
@@ -16,7 +19,8 @@ python hello_world.py
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("<h1>Hello, Tornado</h1>")
+        # self.write("<h1>Hello, Tornado</h1>")
+        self.render("base.html")
 
 
 class SecondHandler(tornado.web.RequestHandler):
@@ -24,11 +28,20 @@ class SecondHandler(tornado.web.RequestHandler):
         self.write("Hello, <h2>Tornado2</h2>")
 
 
+class ErrorHandler(tornado.web.RequestHandler):
+    def get(self):
+        # self.render("500.html", msg=datetime.date.today())
+        self.render("500.html", msg=datetime.datetime.now())
+
+
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
         (r"/2ed", SecondHandler),
-    ])
+        (r"/500", ErrorHandler),
+    ],
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        debug=True)
 
 
 if __name__ == "__main__":

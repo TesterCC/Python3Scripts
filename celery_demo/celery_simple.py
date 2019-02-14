e8fc0718889bcedc9a8380d05f316e03713bd436
yanxi <testerlyx@foxmail.com>
2019-02-14 23:08:48 +0800

    add: update celery demo and heap demo

--- celery_demo/celery_simple.py
+++ celery_demo/celery_simple.py
@@ -3,12 +3,15 @@
 __author__ = 'MFC'
 __time__ = '18/8/23 16:16'
 
+# right demo
+# celery -A celery_simple worker --loglevel=info
+
 from celery import Celery
 
-broker_url = 'amqp://guest:guest@76543210@localhost//'
-backend_url = 'redis://127.0.0.1:6379/0'
+broker_url = 'amqp://yanxi:yanxi@76543210@127.0.0.1:5672/vhost_hdj_email'
+backend_url = 'redis://:HereWithYou@127.0.0.1:6379/2'
 
-app = Celery('hello', broker=broker_url, backend=backend_url)
+app = Celery('celery_simple', broker=broker_url, backend=backend_url)
 # print(dir(app))
 # print(app.IS_macOS)
 # print(app.SYSTEM)
@@ -20,5 +23,7 @@ def hello(args='celery'):
 
 
 if __name__ == '__main__':
-    result = hello('Lily')
-    print(result)
+    list = ["test1","test2","test3"]
+    for arg in list:
+        result = hello.delay(args=arg)
+        print(result)
--- python_cookbook/ch01/1_4_heapq.py
+++ python_cookbook/ch01/1_4_heapq.py
@@ -16,3 +16,25 @@ nums = [18,2,23,7,-4,18,23,42,37,2]
 
 print(heapq.nlargest(3, nums))
 print(heapq.nsmallest(3, nums))
+
+# 两个函数都能接受一个关键字参数，用于更复杂的数据结构中
+
+portfolio = [
+    {'name': 'IBM', 'shares': 100, 'price': 91.1},
+    {'name': 'AAPL', 'shares': 50, 'price': 543.22},
+    {'name': 'FB', 'shares': 200, 'price': 21.09},
+    {'name': 'HPQ', 'shares': 35, 'price': 31.75},
+    {'name': 'YHOO', 'shares': 45, 'price': 16.35},
+    {'name': 'ACME', 'shares': 75, 'price': 115.65},
+]
+
+# 以price的指进行比较
+cheap = heapq.nsmallest(3, portfolio, key=lambda s: s['price'])
+expensive = heapq.nlargest(3, portfolio, key=lambda s: s['price'])
+
+# 以shares的值进行比较
+share = heapq.nsmallest(3, portfolio, key=lambda s: s['shares'])
+
+print(cheap)
+print(expensive)
+print(share)

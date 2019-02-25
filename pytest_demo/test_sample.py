640b7940b9160b903ec9ce3fbfd4fbac210bfa36
yanxi <testerlyx@foxmail.com>
2019-02-25 14:56:59 +0800

    update: pytest demo.

--- pytest_demo/test_sample.py
+++ pytest_demo/test_sample.py
@@ -8,22 +8,28 @@ https://docs.pytest.org/en/latest/
 
 Detail Doc:
 https://docs.pytest.org/en/latest/contents.html#toc
+
+To execute it:
+
+$ pytest
 """
 
 def inc(x):
     return x + 1
 
+def sub(x,y):
+    return x - y
 
-def test_error_answer():
-    assert inc(3) == 5
+
+# def test_error_answer():
+#     assert inc(3) == 5
 
 
 def test_correct_answer():
     assert inc(3) == 4
 
-"""
-To execute it:
 
-$ pytest
-"""
+def test_sub_answer():
+    assert sub(3,-1) == 4
+
 

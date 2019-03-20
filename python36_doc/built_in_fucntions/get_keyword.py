196079ecfb44ec2df34bf291fbe28202b693fca8
yanxi <testerlyx@foxmail.com>
2019-03-21 01:55:38 +0800

    add: print python keyword

---
+++ python36_doc/built_in_fucntions/get_keyword.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'MFC'
+__time__ = '2019-03-21 01:52'
+
+
+from keyword import kwlist
+
+print(kwlist)
+
+# ['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']

aa8d45f6f6ba94891c4af5425130f5fe43f3e12c
yanxi <testerlyx@foxmail.com>
2019-03-25 17:28:15 +0800

    update: get_info_by_url()

--- python3_newtwork_programming/get_info_by_url.py
+++ python3_newtwork_programming/get_info_by_url.py
@@ -12,9 +12,14 @@ from urllib import parse
 def get_info_by_url(url):
     protocol, rest = parse.splittype(url)
     host, path = parse.splithost(rest)
-    return protocol, host, path
+    host, port = parse.splitport(host)
+    if port is None:
+        port = '80'
+    return protocol, host, path, port
 
 
 if __name__ == '__main__':
     url = "https://www.postgresql.org/docs/9.6/index.html"
+    url2 = "https://www.postgresql.org:8000/docs/9.6/index.html"
     print(get_info_by_url(url))
+    print(get_info_by_url(url2))

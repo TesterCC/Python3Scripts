555d8a37fc816d78d8fe335396b4e9722ded630f
yanxi <testerlyx@foxmail.com>
2019-04-15 23:47:22 +0800

    update:mooc python demo week 5

---
+++ mooc_python/programming/lambda_demo.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'MFC'
+__time__ = '2019-04-15 01:23'
+
+f = lambda x,y: x+y
+
+print(f(3,4))
---
+++ mooc_python/programming/week5_nixie_tube.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'MFC'
+__time__ = '2019-04-15 23:41'
+
+import turtle as t
+import time
+
+"""
+要求如下：‪‬‪‬‪‬‪‬‪‬‮‬‫‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‭‬‪‬‪‬‪‬‪‬‪‬‪‬‮‬‫‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‭‬‪‬‪‬‪‬‪‬‪‬‪‬‮‬‪‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‪‬‮‬‪‬‪‬‪‬‪‬‪‬‮‬‭‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‫‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‪‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‫‬‪‬
+
+(1) 使用 time 库获得系统当前时间，格式如下：20190411‪‬‪‬‪‬‪‬‪‬‮‬‪‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‪‬‮‬‪‬‪‬‪‬‪‬‪‬‮‬‭‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‫‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‪‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‫‬‪‬
+
+(2) 绘制对应的七段数码管‪‬‪‬‪‬‪‬‪‬‮‬‪‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‪‬‮‬‪‬‪‬‪‬‪‬‪‬‮‬‭‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‫‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‪‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‫‬‪‬
+
+(3) 数码管风格不限
+
+基本思路：
+
+     步骤 1：绘制单个数字对应的码管
+     步骤 2：获得当前系统时间，变成字符串，绘制对应的码管
+
+思维方法：
+
+     -模块化思维：确定接口，封装功能
+     -规则化思维：抽象过程为规则，计算机自动执行
+     -化繁为简：将大功能变小组合，分而治之
+
+"""
+
+
+def drawGap():  # 绘制数码管间隔
+    t.penup()
+    t.fd(5)
+
+
+def drawLine(draw):  # 绘制单段数码管
+    drawGap()
+    t.pendown() if draw else t.penup()
+    t.fd(40)
+    drawGap()
+    t.right(90)
+
+
+def drawDigit(d):  # 根据数字绘制七段数码管
+    drawLine(True) if d in [2, 3, 4, 5, 6, 8, 9] else drawLine(False)
+    drawLine(True) if d in [0, 1, 3, 4, 5, 6, 7, 8, 9] else drawLine(False)
+    drawLine(True) if d in [0, 2, 3, 5, 6, 8, 9] else drawLine(False)
+    drawLine(True) if d in [0, 2, 6, 8] else drawLine(False)
+    t.left(90)
+    drawLine(True) if d in [0, 4, 5, 6, 8, 9] else drawLine(False)
+    drawLine(True) if d in [0, 2, 3, 5, 6, 7, 8, 9] else drawLine(False)
+    drawLine(True) if d in [0, 1, 2, 3, 4, 7, 8, 9] else drawLine(False)
+    t.left(180)
+    t.penup()
+    t.fd(20)
+
+
+def drawDate(date):
+    t.pencolor("red")
+    for i in date:
+        drawDigit(eval(i))
+
+
+def main():
+    t.setup(800, 350, 200, 200)
+    t.penup()
+    t.fd(-300)
+    t.pensize(5)
+    drawDate(time.strftime('%Y%m%d', time.gmtime()))
+    t.done()
+
+
+main()

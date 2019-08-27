4df97e64ee4abe9dc063179c84b2977cf6fbf53d
TesterCC <testerlyx@foxmail.com>
2019-08-27 11:01:29 +0800

    add: pypinyin demo

--- dev_draft/use_pinyin/pinyin_demo.py
+++ dev_draft/use_pinyin/pinyin_demo.py
@@ -2,3 +2,17 @@
 # -*- coding:utf-8 -*-
 __author__ = 'MFC'
 __time__ = '2019-08-27 10:54'
+
+
+"""
+https://pypinyin.readthedocs.io/zh_CN/master/
+https://github.com/mozillazg/python-pinyin
+"""
+from pypinyin import pinyin, lazy_pinyin
+
+print(pinyin("重庆"))
+print(lazy_pinyin("重庆"))
+print(lazy_pinyin("天津"))
+
+s = "".join(lazy_pinyin("呼和浩特"))
+print(s)

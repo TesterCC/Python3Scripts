17e03bf19ad84dc906bb1a8480f42f968a2e7113
TesterCC <testerlyx@foxmail.com>
2020-09-05 17:08:42 +0800

    add practice

--- fluent_python/2_12_three_x_three.py
+++ fluent_python/2_12_three_x_three.py
@@ -6,4 +6,11 @@ AUTHOR: Yanxi Li
 
 # 2-12 包含3个列表的列表， 嵌套的 3 个列表各自有 3 个元素来代表井字游戏的一行方块
 
-# todo
+board = [['_']*3 for i in range(3)]
+
+print(board)
+
+# 第1行第2列元素标记为X
+board[1][2] = 'X'
+
+print(board)
--- python_cookbook/ch01/1_12_count_max_in_list.py
+++ python_cookbook/ch01/1_12_count_max_in_list.py
@@ -13,4 +13,12 @@ AUTHOR: Yanxi Li
 collections.Counter类的most_common()方法
 """
 
-# todo
+words = ['test', 'look', 'into', 'my', 'eyes', 'look', 'into', 'you', 'understand', 'test', 'look', 'into']
+
+from collections import Counter
+
+word_counts = Counter(words)
+
+# 找出出现频率最高得3个单词
+top_three = word_counts.most_common(3)
+print(top_three)

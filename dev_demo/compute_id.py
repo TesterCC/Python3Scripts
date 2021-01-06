d790533a57b94bdc7811c6907c8f94398585d7a5
TesterCC <testerlyx@foxmail.com>
2021-01-06 11:21:41 +0800

    add: error example to compare

--- dev_demo/compute_id.py
+++ dev_demo/compute_id.py
@@ -9,7 +9,7 @@ import time
 _id_count = 0
 _id_count_time = int(time.time())
 
-
+# 顺序不同会引起_id_count初始值的问题
 def compute__id():
 
     global _id_count, _id_count_time
@@ -24,8 +24,24 @@ def compute__id():
 
     return _id
 
+## 错误示例，仅供对比
+# def compute__id_error():
+#
+#     global _id_count, _id_count_time
+#
+#     _id_count += 1
+#
+#     if time.time() > _id_count_time + 1:
+#         _id_count = 0
+#         _id_count_time = int(time.time())
+#
+#     _id = '{}:{}'.format(_id_count_time, _id_count)
+#
+#     return _id
+
 
 if __name__ == '__main__':
-    print(compute__id())
-    print(compute__id())
-    print(compute__id())
+    for i in range(10):
+        print(compute__id())
+        # print(compute__id_error())
+        time.sleep(0.3)

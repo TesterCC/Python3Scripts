f53be7b1094269f72c7600dac78030d5391aa8ca
testercc <testerlyx@foxmail.com>
2023-05-05 15:24:21 +0800

    updaet - format

--- dev_demo/py7zr/de_demo.py
+++ dev_demo/py7zr/de_demo.py
@@ -1,17 +1,13 @@
 import py7zr
 import os
-password = "01234567" # 密码
-start_dir = "/root/7ztest"  # 要压缩的文件夹路径
-dir_na = "/root/7z.7z" # 压缩后路径
-folder_path = os.path.abspath(start_dir)
-if not os.path.exists(folder_path):
-    os.makedirs(folder_path)
-z7z_file_path = os.path.abspath(os.path.join(folder_path, f'{dir_na}'))
-with py7zr.SevenZipFile(z7z_file_path, mode='w', password=password) as zf:
-    zf.set_encrypted_header(True)
-    for dir_path, dir_names, file_names in os.walk(start_dir):
-        for filename in file_names:
-            fpath = dir_path.replace(start_dir, '')
-            file_path = os.path.join(dir_path, filename)
-            filename = os.path.join(fpath, filename)
-            zf.write(file_path, arcname=filename)
+
+password = "01234567"
+
+
+def zip(path, file, unzip_path):
+    f = py7zr.SevenZipFile(os.path.join(path, file), "r", password=password)
+    f.extractall(unzip_path)
+    # os.remove(os.path.join(path, file))
+
+if __name__ == '__main__':
+    zip("./", "7z.7z", "./un7z")

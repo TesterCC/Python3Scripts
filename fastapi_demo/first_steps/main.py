d145da9a913150357a4a0641c4bff8fe0e322456
TesterCC <testerlyx@foxmail.com>
2023-08-08 11:21:16 +0800

    update - fastapi demo

--- fastapi_demo/first_steps/main.py
+++ fastapi_demo/first_steps/main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
+# default docs:
+# swagger http://127.0.0.1:8000/docs
+# redoc http://127.0.0.1:8000/redoc
 # uvicorn main:app --reload
 # curl http://127.0.0.1:8000
 @app.get("/")

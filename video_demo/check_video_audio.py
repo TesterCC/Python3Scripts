bf3aa05b18662ea3f350f0950d88da41fcbd0493
TesterCC <testerlyx@foxmail.com>
2023-09-04 12:30:39 +0800

    update - some comments

--- video_demo/check_video_audio.py
+++ video_demo/check_video_audio.py
@@ -1,9 +1,9 @@
 from moviepy.editor import VideoFileClip
 
 # pip install moviepy -i https://pypi.tuna.tsinghua.edu.cn/simple
-# 这个示例使用`moviepy`库打开视频文件，并检查是否存在音轨。如果`audio_tracks`不为空，则表示视频包含音轨。
+# 这个示例使用`moviepy`库打开视频文件，并检查是否存在音轨。如果`audio_tracks`不为空，则表示视频包含音轨。  test pass
 
-video_path = r"E:\download_video\bilibili\redteam\xxxx.mp4"
+video_path = r"E:\download_video\xxxx.mp4"
 video = VideoFileClip(video_path)
 audio_tracks = video.audio
 if audio_tracks:

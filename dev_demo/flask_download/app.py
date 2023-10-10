521c6003254f49a83beefcafbca79da48a4c7d39
TesterCC <testerlyx@foxmail.com>
2023-10-10 16:56:05 +0800

    update - support different type file download

--- dev_demo/flask_download/app.py
+++ dev_demo/flask_download/app.py
@@ -1,4 +1,6 @@
 # -*- coding: utf8 -*-
+import time
+
 from flask import Flask, Response, send_file, request
 
 app = Flask(__name__)
@@ -12,17 +14,31 @@ app = Flask(__name__)
 def download_file():
     tid = request.args.get('id')
     print(f"tid: {tid}, {type(tid)}")
-    if tid != "7":
+    if tid == "1":
+
+        # 读取文件a.json的内容
+        with open('./data.json', 'rb') as f:
+            file_data = f.read()
+
+        # 指定响应头，告诉浏览器下载文件，并指定文件名为b.json
+        response = Response(file_data, content_type='application/json')
+        response.headers['Content-Disposition'] = f'attachment; filename={int(time.time())}.json'
+        return response
+    elif tid == "7":
+        # 读取文件内容
+        with open('./test.7z', 'rb') as f:
+            file_data = f.read()
+
+        # 指定响应头，告诉浏览器下载文件，并指定文件类型和文件名
+        headers = {
+            'Content-Type': 'application/x-7z-compressed',
+            'Content-Disposition': f'attachment; filename="{int(time.time())}.7z"',
+        }
+
+        response = Response(file_data, headers=headers)
+        return response
+    else:
         return {}
-    # 读取文件a.json的内容
-    with open('./data.json', 'rb') as f:
-        file_data = f.read()
-
-    # 指定响应头，告诉浏览器下载文件，并指定文件名为b.json
-    response = Response(file_data, content_type='application/json')
-    response.headers['Content-Disposition'] = 'attachment; filename=d.json'
-
-    return response
 
 
 if __name__ == '__main__':

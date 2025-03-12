aa5aa4111048324c210d788b424e674e64df5962
TesterCC <testerlyx@foxmail.com>
2025-03-12 16:22:05 +0800

    add - exponential backoff demo

---
+++ dev_demo/exponential_backoff/retry_demo.py
@@ -0,0 +1,40 @@
+# -*- coding:utf-8 -*-
+'''
+指数退避策略（Exponential Backoff）是一种用于控制重试频率的算法，常见于网络请求、分布式系统或资源竞争场景中。
+其核心思想是：当请求失败时，后续重试的等待时间按指数级增长，从而避免立即重复尝试导致的系统压力或资源耗尽。
+
+常见变种
+
+1.带抖动的退避：在指数时间基础上添加随机波动（如 ±1 秒），避免所有客户端同步重试。
+2.最大等待时间：设置上限（如 30 秒），防止无限等待。
+'''
+
+
+import time
+import random
+
+max_retries = 5 #  最大重试次数
+base_delay = 1  # 初始延迟1秒
+
+def launch_task():
+    # mock task
+    # e.g. url request and so on
+    print("mock run task")
+    # 触发失败后retry
+    return False
+
+for attempt in range(max_retries):
+    try:
+        # 执行请求或操作
+        result = launch_task()
+        if not result:
+            raise RuntimeError
+        break
+    except Exception:
+        if attempt == max_retries - 1:
+            raise "Max retries exceeded"
+        # 计算延迟时间（指数退避 + 抖动）
+        delay = base_delay * (2 ** attempt)
+        delay += random.uniform(0, 1)  # 添加0-1秒的随机抖动
+        print(f"[D] retry delay time: {delay}")
+        time.sleep(delay)
